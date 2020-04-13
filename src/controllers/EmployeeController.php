9f69f001ee40f61bfbe4f929e29e4b2a1c3c7ada
Abdul <abdul@uitouxsolutions.com>
2020-04-13 16:11:07 +0530

    profile image

--- src/controllers/EmployeeController.php
+++ src/controllers/EmployeeController.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace Abs\EmployeePkg;
-use Abs\EmployeePkg\Employee;
-use Abs\EmployeePkg\Designation;
 use Abs\BasicPkg\Attachment;
-use App\ActivityLog;
+use Abs\EmployeePkg\Designation;
+use Abs\EmployeePkg\Employee;
 use App\Http\Controllers\Controller;
 use App\User;
 use Auth;
 use Carbon\Carbon;
 use DB;
-use File;
 use Entrust;
+use File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Validator;
@@ -70,7 +69,7 @@ class EmployeeController extends Controller {
 			})
 			->where(function ($query) use ($request) {
 				if (!empty($request->designation_id)) {
-					$query->where('employees.designation_id',$request->designation_id);
+					$query->where('employees.designation_id', $request->designation_id);
 				}
 			})
 			->where(function ($query) use ($request) {
@@ -111,29 +110,31 @@ class EmployeeController extends Controller {
 			$action = 'Add';
 			$employee->password_change = 'Yes';
 		} else {
-			$employee = Employee::withTrashed()->with('user','employeeAttachment')->find($id);
+			$employee = Employee::withTrashed()->with('user', 'user.profileImage')->find($id);
 			$action = 'Edit';
 			$employee->password_change = 'No';
-			$this->data['employee_attachment']=Attachment::select('name')
-			->where('attachment_of_id',120)//ATTACHMENT OF EMPLOYEE
-			->where('attachment_type_id',140)//ATTACHMENT TYPE OF EMPLOYEE
-			->where('entity_id',$employee->id)
-			->first();
+
+			//issue / saravanan / laravel feature not used
+			// $this->data['employee_attachment'] = Attachment::select('name')
+			// 	->where('attachment_of_id', 120) //ATTACHMENT OF EMPLOYEE
+			// 	->where('attachment_type_id', 140) //ATTACHMENT TYPE OF EMPLOYEE
+			// 	->where('entity_id', $employee->user->id)
+			// 	->first();
 		}
 		$this->data['success'] = true;
 		$this->data['employee'] = $employee;
-		$this->data['designation_list'] =collect(Designation::select('name', 'id')->where('company_id', Auth::user()->company_id)->get())->prepend(['id' => '', 'name' => 'Select Designation']);
+		$this->data['designation_list'] = collect(Designation::select('name', 'id')->where('company_id', Auth::user()->company_id)->get())->prepend(['id' => '', 'name' => 'Select Designation']);
 		$this->data['action'] = $action;
 		return response()->json($this->data);
 	}
 	public function getEmployeeFilterData() {
-		$this->data['designation_list'] =collect(Designation::select('name', 'id')->where('company_id', Auth::user()->company_id)->get())->prepend(['id' => '', 'name' => 'Select Designation']);
+		$this->data['designation_list'] = collect(Designation::select('name', 'id')->where('company_id', Auth::user()->company_id)->get())->prepend(['id' => '', 'name' => 'Select Designation']);
 		$this->data['success'] = true;
 		return response()->json($this->data);
 	}
 
 	public function saveEmployee(Request $request) {
-		 //dd($request->all());
+		//dd($request->all());
 		try {
 			$error_messages = [
 				'code.required' => 'Code is Required',
@@ -212,7 +213,7 @@ class EmployeeController extends Controller {
 					'min:3',
 					'max:32',
 					'unique:users,username,' . $request->id . ',entity_id',
-				],	
+				],
 			], $user_error_messages);
 			if ($user_validator->fails()) {
 				return response()->json(['success' => false, 'errors' => $user_validator->errors()->all()]);
@@ -231,7 +232,7 @@ class EmployeeController extends Controller {
 					'entity_id' => $request->id,
 					'user_type_id' => 1,
 				])
-				->first();
+					->first();
 				$user->updated_by_id = Auth::user()->id;
 			}
 			$employee->fill($request->all());
@@ -252,11 +253,11 @@ class EmployeeController extends Controller {
 			$user->save();
 
 			//Employee Profile Attachment
-			$employee_images_des = storage_path('app/public/employee/attachments/');
+			$employee_images_des = storage_path('app/public/user-profile-images/');
 			//dump($employee_images_des);
 			Storage::makeDirectory($employee_images_des, 0777);
 			if (!empty($request['attachment'])) {
-				if(!File::exists($employee_images_des)) {
+				if (!File::exists($employee_images_des)) {
 					File::makeDirectory($employee_images_des, 0777, true);
 				}
 				$remove_previous_attachment = Attachment::where([
@@ -265,7 +266,7 @@ class EmployeeController extends Controller {
 					'attachment_type_id' => 140,
 				])->first();
 				if (!empty($remove_previous_attachment)) {
-					$img_path = $employee_images_des.$remove_previous_attachment->name;
+					$img_path = $employee_images_des . $remove_previous_attachment->name;
 					if (File::exists($img_path)) {
 						File::delete($img_path);
 					}
@@ -273,25 +274,24 @@ class EmployeeController extends Controller {
 				}
 
 				/*$exists_path=storage_path('app/public/employee/attachments/'.$employee->id.'/');
-				//dd($exists_path);
-				if (is_dir($exists_path))
-				{
-					unlink($exists_path);
-				}*/
+					//dd($exists_path);
+					if (is_dir($exists_path))
+					{
+						unlink($exists_path);
+				*/
 				$extension = $request['attachment']->getClientOriginalExtension();
-				$request['attachment']->move(storage_path('app/public/employee/attachments/'), $employee->id .'.'.$extension);
+				$request['attachment']->move(storage_path('app/public/user-profile-images/'), $user->id . '.' . $extension);
 				$employee_attachement = new Attachment;
 				$employee_attachement->company_id = Auth::user()->company_id;
 				$employee_attachement->attachment_of_id = 120; //ATTACHMENT OF EMPLOYEE
 				$employee_attachement->attachment_type_id = 140; //ATTACHMENT TYPE  EMPLOYEE
-				$employee_attachement->entity_id = $employee->id;
-				$employee_attachement->name = $employee->id .'.'.$extension;
+				$employee_attachement->entity_id = $user->id;
+				$employee_attachement->name = $user->id . '.' . $extension;
 				$employee_attachement->save();
-				$user->profile_image_id=$employee_attachement->id;
+				$user->profile_image_id = $employee_attachement->id;
 				$user->save();
 
 			}
-
 
 			// $activity = new ActivityLog;
 			// $activity->date_time = Carbon::now();
@@ -334,18 +334,18 @@ class EmployeeController extends Controller {
 					'entity_id' => $request->id,
 					'user_type_id' => 1,
 				])
-				->forceDelete();
+					->forceDelete();
 				$employee = Employee::withTrashed()->where('id', $request->id)->forceDelete();
 				/*$activity = new ActivityLog;
-				$activity->date_time = Carbon::now();
-				$activity->user_id = Auth::user()->id;
-				$activity->module = 'Employees';
-				$activity->entity_id = $request->id;
-				$activity->entity_type_id = 1420;
-				$activity->activity_id = 282;
-				$activity->activity = 282;
-				$activity->details = json_encode($activity);
-				$activity->save();*/
+					$activity->date_time = Carbon::now();
+					$activity->user_id = Auth::user()->id;
+					$activity->module = 'Employees';
+					$activity->entity_id = $request->id;
+					$activity->entity_type_id = 1420;
+					$activity->activity_id = 282;
+					$activity->activity = 282;
+					$activity->details = json_encode($activity);
+				*/
 
 				DB::commit();
 				return response()->json(['success' => true, 'message' => 'Employee Deleted Successfully']);
