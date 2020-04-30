bd7d8ffaca9c06eb06b0096e393c3b42527f4fa9
Abdul <abdul@uitouxsolutions.com>
2020-04-30 10:23:51 +0530

    skill level setup

---
+++ src/models/SkillLevel.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Abs\EmployeePkg;
+
+use Abs\HelperPkg\Traits\SeederTrait;
+use App\Company;
+use App\Config;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SkillLevel extends Model {
+	use SeederTrait;
+	use SoftDeletes;
+	protected $table = 'skill_levels';
+	public $timestamps = true;
+	protected $fillable = [
+		'company_id',
+		'short_name',
+		'name',
+		'description',
+	];
+
+	public function employees() {
+		return $this->hasMany('App\Employee', 'skill_level_id');
+	}
+
+	public static function createFromObject($record_data) {
+
+		$errors = [];
+		$company = Company::where('code', $record_data->company)->first();
+		if (!$company) {
+			dump('Invalid Company : ' . $record_data->company);
+			return;
+		}
+
+		$admin = $company->admin();
+		if (!$admin) {
+			dump('Default Admin user not found');
+			return;
+		}
+
+		$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
+		if (!$type) {
+			$errors[] = 'Invalid Tax Type : ' . $record_data->type;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data->tax_name,
+		]);
+		$record->type_id = $type->id;
+		$record->created_by_id = $admin->id;
+		$record->save();
+		return $record;
+	}
+
+}
