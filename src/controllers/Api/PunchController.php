2ce5c280117265ad15857f16ee5aa91405ecd8f3
saravana-uitoux <saravanan@uitouxsolutions.com>
2020-05-12 15:09:22 +0530

    punch in and out  changes

--- src/controllers/Api/PunchController.php
+++ src/controllers/Api/PunchController.php
@@ -14,13 +14,8 @@ use Validator;
 class PunchController extends Controller {
 	public $successStatus = 200;
 
-	public function status(Request $request) {
+	public function punch(Request $request) {
 		try {
-
-			/*if (!Entrust::can('punch')) {
-				return response()->json(['success' => false, 'message' => 'Permission Denied!'], $this->successStatus);
-			}*/
-
 			$validator = Validator::make($request->all(), [
 				'encrypted_id' =>[
 				 	'required',
@@ -29,7 +24,6 @@ class PunchController extends Controller {
 				 ],
 			]);
 			if ($validator->fails()) {
-				// dd($validator->errors()->all()->toArray());
 				return response()->json([
 					'success' => false,
 					'message' => 'Validation Error',
@@ -38,121 +32,66 @@ class PunchController extends Controller {
 				// issue : saravanan : readability
 				// return response()->json(['success' => false, 'message' => 'Validation Error', 'errors' => $validator->errors()], $this->successStatus);
 			}
-			$logged_user = User::find(Auth::user()->id);
-			if (!$logged_user) {
-				return response()->json([
-					'status' => false,
-					'message' => 'Logged user not found',
-				], $this->successStatus);
-			}
-			$logged_user->employee->outlet;
-
 			$user = User::find($request->encrypted_id);
 			if (!$user) {
 				// issue : saravanan : readability
 				return response()->json([
 					'status' => false,
-					'message' => 'Employee not found',
+					'message' => 'User not found',
 				], $this->successStatus);
 				// return response()->json(['status' => false, 'message' => 'Employee not found'], $this->successStatus);
 			}
 
-			$punch = AttendanceLog::whereDate('date', date('Y-m-d'))->where('user_id', $user->id)->orderBy('id', 'DESC')->first();
-			if ($punch) {
-				if (!$punch->out_time) {
+			$punch = AttendanceLog::whereDate('date', date('Y-m-d'))
+			->where('user_id', $user->id)
+			->whereNull('out_time')
+			->orderBy('id', 'DESC')
+			->first();
+			if ($punch) {//PUNCH OUT
 					$action = "Out";
 					$data['punch_in'] = $punch;
 					$data['punch_out_method_list']=PunchOutMethod::getList();
-				} else {
-					$action = "In";
-				}
-			} else {
+				
+			} else {//PUNCH IN
+				$punch_in = new AttendanceLog();
+				$punch_in->date = $date = date('Y-m-d');
+				$time = date('H:i:s');
+				$punch_in_time = strtotime($time);
+				$punch_in->in_time = date('H:i:s', $punch_in_time);
+				$punch_in->user_id = $user->id;
+				$punch_in->created_by_id = Auth::id();
+				$punch_in->save();
 				$action = "In";
+				//dd($punch_in);
+				$data['punch_in'] = $punch_in;
 			}
 			$data['action'] = $action;
 			$data['date'] = date('Y-m-d');
 			$data['time'] = date('h:i a');
-			//$user->profile_image_url = $user->profile_image_url;
+			$user->employee->outlet;
 			$data['user'] = $user;
-			$data['logged_user'] = $logged_user;
-			//$data['punch_out_method_list']=PunchOutMethod::getList();
-			//dd($data);
-			return response()->json(['success' => true, 'data' => $data], $this->successStatus);
-		} catch (Exception $e) {
-			return response()->json(['status' => false, 'message' => 'Punch ' . $type . ' failed!'], $this->successStatus);
-		}
-	}
-
-	public function savePunchIn(Request $request) {
-		try {
-
-			/*if (!Entrust::can('punch')) {
-				return response()->json(['success' => false, 'message' => 'Permission Denied!'], $this->successStatus);
-			}*/
-
-			$validator = Validator::make($request->all(), [
-				'encrypted_id' => [
-					'required',
-					'numeric',
-					'exists:users,id',
-				],
-			]);
-			if ($validator->fails()) {
+			if($action=='In')
+			{
 				return response()->json([
-					'success' => false,
-					'errors' => $validator->errors()->all(),
+					'success' => true,
+					'message' => 'Punch ' . $action . ' success!',
+					'data' => $data
 				], $this->successStatus);
 			}
-
-			//$user_id = Crypt::decrypt($request->qr_code);
-			$user = User::find($request->encrypted_id);
-			//dd($user);
-			if (!$user) {
-				return response()->json([
-					'success' => false, 'message' => 'Employee not found'], $this->successStatus);
-			}
-
-			//$logged_in_by_id = $request->user()->id;
-			//dd($logged_in_by_id);
-			DB::beginTransaction();
-
-			$punch = AttendanceLog::whereDate('date', date('Y-m-d'))->where('user_id', $user->id)->whereNull('out_time')->first();
-			if($punch){
-				return response()->json([
-					'success' => false,
-					 'message' => 'Punch in details already exists'
-				], $this->successStatus);
-			}
-		
-			$punch_in = new AttendanceLog();
-			$punch_in->date = $date = date('Y-m-d');
-			$time = date('H:i:s');
-			$punch_in_time = strtotime($time) - (10 * 60);
-			$punch_in->in_time = date('H:i:s', $punch_in_time);
-			$punch_in->user_id = $user->id;
-			$punch_in->created_by_id = Auth::id();
-			$punch_in->save();
-			$type = 'In';
-			$data['user'] = $user;
-			$data['action'] = 'In';
-			DB::commit();
 			return response()->json([
-				'success' => true, 
-				'message' => 'Punch ' . $type . ' success!',
-				'data'=>$data,
+				'success' => true,
+				'data' => $data
 			], $this->successStatus);
 		} catch (Exception $e) {
-			DB::rollBack();
-			return response()->json(['success' => false, 'message' => 'Punch ' . $type . ' failed!'], $this->successStatus);
+			return response()->json([
+				'status' => false,
+				'message' => 'Punch in failed!'
+			], $this->successStatus);
 		}
-
 	}
+
 	public function savePunchOut(Request $request) {
 		try {
-
-			/*if (!Entrust::can('punch')) {
-				return response()->json(['success' => false, 'message' => 'Permission Denied!'], $this->successStatus);
-			}*/
 
 			$validator = Validator::make($request->all(), [
 				'encrypted_id' => [
@@ -169,7 +108,6 @@ class PunchController extends Controller {
 					'nullable',
 					'string',
 					'max:255',
-					'exists:punch_out_methods,id',
 				],
 			]);
 			if ($validator->fails()) {
@@ -178,19 +116,14 @@ class PunchController extends Controller {
 					'errors' => $validator->errors()->all(),
 				], $this->successStatus);
 			}
-			//$user_id = Crypt::decrypt($request->qr_code);
 			$user = User::find($request->encrypted_id);
-			//dd($user);
 			if (!$user) {
 				return response()->json([
 					'success' => false,
-					 'message' => 'Employee not found'
+					 'message' => 'User not found'
 				], $this->successStatus);
 			}
-
-			//$logged_in_by_id = $request->user()->id;
-			//dd($logged_in_by_id);
-
+		
 			$punch = AttendanceLog::whereDate('date', date('Y-m-d'))->where('user_id', $user->id)->whereNull('out_time')->first();
 			if (!$punch) {
 				return response()->json([
@@ -213,7 +146,7 @@ class PunchController extends Controller {
 				$punch->updated_by_id = Auth::id();
 				$date = date('Y-m-d');
 				$punch->save();
-				$type = 'Out';
+				$user->employee->outlet;
 				$data['user'] = $user;
 				$data['action'] = 'Out';
 				$data['punch_out'] = $punch;
@@ -221,14 +154,14 @@ class PunchController extends Controller {
 			DB::commit();
 			return response()->json([
 				'success' => true,
-				'message' => 'Punch ' . $type . ' success!',
+				'message' => 'Punch ' . $data['action'] . ' success!',
 				'data'=>$data
 			], $this->successStatus);
 		} catch (Exception $e) {
 			DB::rollBack();
 			return response()->json([
 				'success' => false,
-				 'message' => 'Punch ' . $type . ' failed!'
+				 'message' => 'Punch  Out failed!'
 			], $this->successStatus);
 		}
 
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 Route::group(['namespace' => 'Abs\EmployeePkg\Api', 'middleware' => ['api', 'auth:api']], function () {
 	Route::group(['prefix' => 'api/employee-pkg'], function () {
-		Route::post('punch-in/save', 'PunchController@savePunchIn')->middleware('auth:api');
-		Route::post('punch/status', 'PunchController@status');
+		
+		Route::post('punch', 'PunchController@punch');
 		Route::post('punch-out/save', 'PunchController@savePunchOut')->middleware('auth:api');
 	});
 });
